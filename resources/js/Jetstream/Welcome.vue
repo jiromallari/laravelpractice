31fcbbc6f506da05992fd55fe99673462e426c2a
jiromallari <77607278+jiromallari@users.noreply.github.com>
2021-06-09 20:59:13 +0800

    Test First Commit

---
+++ resources/js/Jetstream/Welcome.vue
@@ -0,0 +1,119 @@
+<template>
+    <div>
+        <div class="p-6 sm:px-20 bg-blue-200 border-b border-black-500">
+
+            <div class="mt-8 text-2xl text-red-500 font-bold">
+                <center>
+                Welcome to Gallerina!
+                </center>
+            </div>
+
+            <div class="mt-6 text-black-500">
+                <center>
+                The National Gallerina of Art, founded as a gift to the nation, serves as a center 
+                of visual art, education, and culture. Our collection of more than 150,000 paintings,
+                sculpture, decorative arts, photographs, prints, and drawings spans the history of 
+                Western art and showcases some of the triumphs of human creativity. Across 363 days a 
+                year, the Gallery offers a full spectrum of special exhibitions and public programs free of charge.
+                </center>
+            </div>
+        </div>
+
+        <div class="bg-blue-300 bg-opacity-40 x">
+            <div class="p-6">
+                <div class="flex items-center">
+                   
+                    <div class="ml-4 text-lg text-red-500 leading-7 font-bold">HISTORY</div>
+                </div>
+
+                <div class="ml-12">
+                    <div class="mt-2 text-sm text-black-500">
+                        
+                        The National Gallerina of Art was conceived and given to the people of the United States 
+                        by Andrew W. Mellon, a financier and art collector who served as secretary of the treasury 
+                        under four presidents from 1921 to 1932. During his years as a public servant, Mellon came to 
+                        believe that the United States should have a world-class national art museum comparable to those 
+                        of other nations. In 1936 Mellon wrote to President Franklin D. Roosevelt to offer his gift of paintings 
+                        and sculpture for a new museum in Washington, DC, that he would build and finance with his own funds. 
+                        Roosevelt endorsed Mellon’s offer, and Congress accepted his gift in 1937.
+                       
+                    </div>
+
+                    
+                        <div class="mt-3 flex items-center text-sm font-bold text-indigo-700">
+                                <div>Read History</div>
+
+                                <div class="ml-1 text-indigo-500">
+                                    <svg viewBox="0 0 20 20" fill="currentColor" class="w-4 h-4"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                                </div>
+                        </div>
+                   
+                </div>
+            </div>
+
+            <div class="p-6 border-t border-gray-200 md:border-t-0 md:border-l">
+                <div class="flex items-center">
+                    
+                    <div class="ml-4 text-lg text-red-500 leading-7 font-bold ">ARTINIZE</div>
+                </div>
+
+                <div class="ml-12">
+                    <div class="mt-2 text-sm text-black-500">
+                        The Artinize is an Art and Design consultancy and online gallery focused on highlighting the best
+                        of contemporary art and design from both emerging and established studios and galleries across the globe.
+                        Our online platform, theArtling.com, now lists over 3,000 designers, artists and galleries from around the 
+                        world, with new artworks and design items added to the site daily.
+                    </div>
+
+                    
+                        <div class="mt-3 flex items-center text-sm font-bold text-indigo-700">
+                                <div>See Artinize</div>
+
+                                <div class="ml-1 text-indigo-500">
+                                    <svg viewBox="0 0 20 20" fill="currentColor" class="w-4 h-4"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                                </div>
+                        </div>
+                    
+                </div>
+            </div>
+
+            <div class="p-6 border-t border-gray-200">
+                <div class="flex items-center">
+                    
+                    <div class="ml-4 text-lg text-red-500 leading-7 font-bold">ABOUT US</div>
+                </div>
+
+                <div class="ml-12">
+                    <div class="mt-2 text-sm text-black-500">
+                        Everything we do makes the business of art more efficient, giving you the time and space to do the things you love. Whether that’s creating more artwork, discovering new opportunities and artists, growing your collection or simply getting organised. We future-proof your systems, coordinate your marketing and modernise your business. By simplifying and streamlining your systems we save you time and money. In the gallery, the studio or at art fairs, you’re always able to easily and elegantly organise, manage and share.
+                    </div>
+                </div>
+            </div>
+
+            <div class="p-6 border-t border-gray-200 md:border-l">
+                <div class="flex items-center">
+                    
+                    <div class="ml-4 text-lg text-red-500 leading-7 font-bold">CONTACT DETAILS</div>
+                </div>
+
+                <div class="ml-12">
+                    <div class="mt-2 text-sm text-black-500">
+                       Name: John Rogel Mallari 
+                       <div> Contact Number: 09057869758 </div>
+                       <div> Email: Mjrdcmallari@tip.edu.ph </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import JetApplicationLogo from '@/Jetstream/ApplicationLogo'
+
+    export default {
+        components: {
+            JetApplicationLogo,
+        },
+    }
+</script>
